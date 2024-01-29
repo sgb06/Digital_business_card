53bed813ece6c6573bf863131a0d1b044841ba60
sgb06 <santigallego2706@gmail.com>
2024-01-28 20:14:33 -0500

    add new aboutMe component

---
+++ src/components/aboutme/AboutMe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        "I am a person willing to learn new things every day, with good ability
+        to adapt to work environments, I enjoy working in teams and providing
+        possible solutions."
+      </p>
+      
+    </div>
+
+  );
+};
