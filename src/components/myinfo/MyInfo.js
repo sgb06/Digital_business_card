2a6cb7b5bd58bc34c4abbfeb6bd8c9b3434e3d97
sgb06 <santigallego2706@gmail.com>
2024-01-28 22:12:22 -0500

    add new MyInfo component

---
+++ src/components/myinfo/MyInfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MyInfo.css";
+
+const MyInfo = () => {
+  return (
+    <div className="Information">
+      <h1>Santiago Gallego</h1>
+      <h2>Developer</h2>
+      <h3>santigallego2706@gmail.com</h3>
+    </div>
+  );
+};
+
+export default MyInfo;
